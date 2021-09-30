c07028a4d1f11e0bf86a4afa0b8d86ec0cc624aa
Manu Glez <manuaglez@gmail.com>
2021-09-30 10:01:25 +0100

    testing 2

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+const { resolve } = require("path");
 const path = require("path");
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : "/";
